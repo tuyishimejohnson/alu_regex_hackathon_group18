0461fc012729f6ebba6d17cb665d0b709c082c19
Beritha-n12 <116920809+Beritha-n12@users.noreply.github.com>
2023-05-24 09:15:29 +0000

    matching dates code

--- dates.py
+++ dates.py
@@ -1,17 +1,17 @@
 import re
 
-def validate_date(date_string):
-    pattern = r"\d{2}-[A-Za-z]{3}-\d{4}"
-    if re.match(pattern, date_string):
+def verify_day(day_string):
+    input = r"\d{2}-[B-Za-z]{3}-\d{4}"
+    if re.match(input, day_string):
         return True
     else:
         return False
 
 # by using those examples
-date1= "24-May-2023"
-date2= "2023-May-24"
-date3= "April-24-2023"
+day1= "24-May-2023"
+day2= "2023-May-24"
+day3= "May-24-2023"
 
-print(validate_date(date1)) #output: True
-print(validate_date(date2)) #output: False
-print(validate_date(date3)) #output: False
+print(verify_day(day1)) #output: True
+print(verify_day(day2)) #output: False
+print(verify_day(day3)) #output: False
